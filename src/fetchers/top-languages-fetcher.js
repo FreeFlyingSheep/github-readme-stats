2cdbf3dc26e07af80a3e095fcc1ccf75fb83c2d7
Feiyang Chen <34179926+FreeFlyingSheep@users.noreply.github.com>
2022-08-16 09:46:23 +0800

    Update top-languages-fetcher.js

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -13,11 +13,11 @@ const fetcher = (variables, token) => {
       query: `
       query userInfo($login: String!) {
         user(login: $login) {
-          # fetch only owner repos & not forks
-          repositories(ownerAffiliations: OWNER, isFork: true, first: 3) {
+          # fetch only owner repos & forks
+          repositories(ownerAffiliations: OWNER, isFork: true, first: 100) {
             nodes {
               name
-              languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+              languages(first: 3, orderBy: {field: SIZE, direction: DESC}) {
                 edges {
                   size
                   node {
